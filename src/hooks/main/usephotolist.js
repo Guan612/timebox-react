287f1afa781b4c9099a6af5613e8af9281c0ea6b
Guan612 <tiantony53@163.com>
2024-06-02 16:39:53 +0800

    新增时间线假数据

--- src/hooks/main/usephotolist.js
+++ src/hooks/main/usephotolist.js
@@ -1,8 +1,20 @@
 import { useState, useEffect } from "react";
 import { getPhotoListAPI } from "../../api/photo";
+import { Children } from "react";
 
 export default function usePhotoList() {
     const [photoList, setPhotoList] = useState([]);
+    const times = [
+        {
+            children:2024,
+        },
+        {
+            children:2023,
+        },
+        {
+            children:2022,
+        }
+    ]
     const getPhotoList = async()=> {
         const {reslut} = await getPhotoListAPI();
         setPhotoList(reslut);
@@ -11,5 +23,5 @@ export default function usePhotoList() {
         getPhotoList();
     }, []);
 
-    return {photoList};
+    return {photoList,times};
 }
