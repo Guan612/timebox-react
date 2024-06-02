23843d36b870ad80a0a279d1a0e40d06845198ae
Guan612 <tiantony53@163.com>
2024-06-02 16:40:21 +0800

    引入首页照片卡片

---
+++ src/pages/main/index.jsx
@@ -0,0 +1,23 @@
+import { Timeline } from "antd";
+import usePhotoList from "../../hooks/main/usephotolist";
+import PhotoCard from "./photocard";
+export default function Main() {
+    const { photoList, times } = usePhotoList();
+    return (
+        <div className="flex flex-row">
+            <div className="justify-center grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 items-center">
+                {photoList.map((item, index) => {
+                    return (
+                        <PhotoCard
+                            photoUrl={item.photoUrl}
+                            photoName={item.photoName}
+                            photoDes={item.photoDes}
+                            key={index}
+                        />
+                    );
+                })}
+            </div>
+            <Timeline items={times}></Timeline>
+        </div>
+    );
+}
