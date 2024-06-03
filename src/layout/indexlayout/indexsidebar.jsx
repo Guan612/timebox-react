40b0e77e27bef0f76d55b588f7b90dd4a40ed2f1
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-06-03 16:03:59 +0800

    修改侧边链接

--- src/layout/indexlayout/indexsidebar.jsx
+++ src/layout/indexlayout/indexsidebar.jsx
@@ -1,18 +1,19 @@
+import {NavLink} from 'react-router-dom'
 export default function IndexSidebar() {
     return (
         <div className="flex flex-col bg-transblue h-screen justify-between">
             <div className="mt-5">
-                <div className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
+                <NavLink to={"/"}  className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
                     首页
+                </NavLink >
+                <div className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
+                    我的照片集
                 </div>
+                <NavLink to={"/photos"} className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
+                    我的照片
+                </NavLink>
                 <div className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
-                    首页
-                </div>
-                <div className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
-                    首页
-                </div>
-                <div className="m-5 p-3 bg-transblue rounded-xl hover:bg-transpink transition-all duration-300 cursor-pointer">
-                    首页
+                    最爱精选
                 </div>
             </div>
         </div>
