ae806388e00357ac5ba5b3fa409b76765e55ef04
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-06-04 09:46:05 +0800

    修改照片页面

--- src/pages/photos/index.jsx
+++ src/pages/photos/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Image } from "antd";
+import { Image, message, Popconfirm, Drawer } from "antd";
 import { PlusCircleOutlined, DeleteOutlined } from "@ant-design/icons";
 import {
     findMyPhotoAPI,
@@ -11,10 +11,27 @@ import {
 
 export default function Photos() {
     const [photoList, setPhotoList] = useState([]);
+    const [open, setOpen] = useState(false);
 
     const getMyPhoto = async () => {
         const { reslut } = await findMyPhotoAPI();
         setPhotoList(reslut);
+    };
+
+    const confirm = (e) => {
+        console.log(e);
+        setOpen(true);
+    };
+    const cancel = (e) => {
+        console.log(e);
+        message.error("Click on No");
+    };
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
     };
 
     useEffect(() => {
@@ -23,15 +40,35 @@ export default function Photos() {
 
     return (
         <div className="flex flex-row m-5 justify-center">
+            <Drawer title="照片详细" onClose={onClose} open={open}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Drawer>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {photoList.map((photo) => {
                     return (
-                        <img
-                            className="rounded-lg shadow-lg cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out"
-                            src={photo.photoUrl}
-                            style={{ height: 230 }}
-                            key={photo.id}
-                        ></img>
+                        <div key={photo.id}>
+                            <Popconfirm
+                                title="详情"
+                                description={
+                                    photo.photoAndColl.length > 0
+                                        ? photo.photoAndColl[0].photoCollection
+                                              .photoName
+                                        : "还未加入照片集"
+                                }
+                                onConfirm={confirm}
+                                onCancel={cancel}
+                                okText="详细信息"
+                                cancelText="取消"
+                            >
+                                <img
+                                    className="rounded-lg shadow-lg cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out"
+                                    src={photo.photoUrl}
+                                    style={{ height: 230 }}
+                                ></img>
+                            </Popconfirm>
+                        </div>
                     );
                 })}
             </div>
