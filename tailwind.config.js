cbf0f98001d8d1989eec55662f3b756bce87f382
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-09-11 12:27:14 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-        colors: {
-            "transblue": "#5BCEFA",
-            "transpink": "#F5A9B8",
-        }
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      transblue: "#5BCEFA",
+      transpink: "#F5A9B8",
     },
-    plugins: [],
+  },
+  plugins: [],
 };
